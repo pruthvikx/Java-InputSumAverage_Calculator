10efb0235b0d9856e8bc24273c75c4fa03b45f5c
Pruthvi Kolhe <65055563+pruthvikx@users.noreply.github.com>
2022-04-30 21:21:00 +0530

    Input Numbers , break and return sum and average value of all input numbers if other than integer value is input

---
+++ src/InputCalculator.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class InputCalculator {
+    public static void inputThenPrintSumAndAverage() {
+        Scanner scanner = new Scanner(System.in);
+        double sum = 0;
+        double avg = 0;
+        int count = 0;
+
+
+        while (true) {
+            boolean isInt = scanner.hasNextInt();
+
+            if (isInt) {
+                int number = scanner.nextInt();
+                sum += number;
+                count++;
+
+            } else {
+                break;
+            }
+        }
+        scanner.nextLine();
+        avg = (sum / count);
+
+        System.out.println("SUM = " + Math.round(sum) + " AVG = " + Math.round(avg));
+        scanner.close();
+
+    }
+
+}
